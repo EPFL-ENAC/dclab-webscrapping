3530621e12d1b7076ab1ece21dc554afa7e0dec4
Son Pham-Ba <son.phamba@epfl.ch>
2025-03-17 17:26:19 +0100

    feat: add instagram scrapping

--- README.md
+++ README.md
@@ -12,10 +12,22 @@ pip install -e .
 ## Processing État de Vaud's "Registre du commerce"
 
 Script: `./webscrapping/process/vd.py`
+
 Create a `.env` file with the following content:
 ```
-GOOGLE_MAPS_API_KEY=...
-OLLAMA_API_URL=...
+GOOGLE_MAPS_API_KEY="..."
+OLLAMA_API_URL="..."
 ```
 
 You may install Ollama locally to use a LLM on your local machine. In this case, the `OLLAMA_API_URL` should look like `http://localhost:11434`.
+
+
+## Scrapping Instagram
+
+Script: `./webscrapping/scrap/instagram.py`
+
+Create a `.env` file with the following content:
+```
+INSTAGRAM_USERNAME="..."
+INSTAGRAM_PASSWORD="..."
+```
---
+++ webscrapping/scrap/instagram.py
@@ -0,0 +1,44 @@
+import os
+
+from dotenv import load_dotenv
+from instagrapi import Client
+
+
+load_dotenv()
+INSTAGRAM_USERNAME = os.getenv("INSTAGRAM_USERNAME")
+INSTAGRAM_PASSWORD = os.getenv("INSTAGRAM_PASSWORD")
+
+
+BASE_HASHTAG = "suisseromande"
+SEARCH_HASHTAGS = ["handmade", "homemade"]
+SEARCH_TEXTS = ["dm for order", "purchase"]
+MAX_POSTS = 10
+
+
+print("Logging in to Instagram...")
+client = Client()
+client.login(INSTAGRAM_USERNAME, INSTAGRAM_PASSWORD)
+print("Logged!")
+
+
+def find_users(client: Client, base_hashtag: str, search_hashtags: list[str], search_texts: list[str], amount: int = 10) -> set[str]:
+    posts = client.hashtag_medias_top(base_hashtag, amount)
+    hashtags_and_texts = [f"#{hashtag}" for hashtag in search_hashtags] + search_texts
+    usernames = set()
+
+    for post in posts:
+        post_text = post.caption_text.lower()
+        print(f"Checking post by {post.user.username}", flush=True)
+
+        if any(t in post_text for t in hashtags_and_texts):
+            usernames.add(post.user.username)
+            print(f"Match found: {post_text}", flush=True)
+            continue
+
+    return usernames
+
+
+matching_users = find_users(client, BASE_HASHTAG, SEARCH_HASHTAGS, SEARCH_TEXTS, MAX_POSTS)
+
+print("\nMatching users:")
+print(matching_users)
